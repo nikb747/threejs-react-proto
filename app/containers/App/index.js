f0cabb96b18a49d73cedda6c2205f6a294c4f2cc
Nik Billings <nbillings@containerstore.com>
2017-06-02 11:27:57 -0500

    Added missing three dependency, added example code

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -12,17 +12,39 @@
  */
 
 import React from 'react';
+import React3 from 'react-three-renderer';
+import * as THREE from 'three';
 
-export default class App extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+export default class App extends React.PureComponent {
+  // eslint-disable-line react/prefer-stateless-function
 
   static propTypes = {
-    children: React.PropTypes.node,
+    children: React.PropTypes.node
   };
+  cameraPosition = new THREE.Vector3(0, 0, 5);
 
   render() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
     return (
       <div>
         {React.Children.toArray(this.props.children)}
+        <React3 mainCamera="camera" width={width} height={height}>
+          <scene>
+            <perspectiveCamera
+              name="camera"
+              fov={75}
+              aspect={width / height}
+              near={0.1}
+              far={1000}
+              position={this.cameraPosition}
+            />
+            <mesh>
+              <boxGeometry width={1} height={1} depth={1} />
+              <meshBasicMaterial color={0x00ff00} />
+            </mesh>
+          </scene>
+        </React3>
       </div>
     );
   }
